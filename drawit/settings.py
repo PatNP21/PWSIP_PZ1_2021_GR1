0653ad533f7d32f587f826b19f8ce73a21c8e06e
feelhardcore <tomaszgodlewski01@gmail.com>
2021-11-09 01:11:00 +0100

    Changed to PGSQL

--- drawit/settings.py
+++ drawit/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'drawit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'drawit_server',
+
+        'USER': 'admin',
+
+        'PASSWORD': 'admin',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '5432',
     }
 }
 
