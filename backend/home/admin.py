ddfb9c63aa4308312c961f6ef61d85a5584d9078
feelhardcore <tomaszgodlewski01@gmail.com>
2021-12-11 22:43:58 +0100

    dodanko do adminka

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from home.models import User, Session
+from home.models import User, Session, UserActivation
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Session)
+admin.site.register(UserActivation)
