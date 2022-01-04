c03f2088b987e94a3a44bc7e5d2a2b789ad6df40
NeverCarelessWhisper <pabelo05@o2.pl>
2022-01-04 14:15:14 +0100

    styl karty

--- frontend/src/components/PostList/PostItem.js
+++ frontend/src/components/PostList/PostItem.js
@@ -3,18 +3,20 @@ import classes from "./PostItem.module.css";
 function PostItem(props) {
   return (
     <li>
+      
       <div className={classes.image}>
         <img src={props.image} alt={props.title} />
       </div>
       <div className={classes.content}>
         <h3>{props.title}</h3>
         <address>{props.address}</address>
-        <p>{props.descryption}</p>
+        <p>{props.description}</p>
       </div>
       <div className={classes.actions}>
         <button>To Favourites</button>
       </div>
     </li>
+
   );
 }
 
