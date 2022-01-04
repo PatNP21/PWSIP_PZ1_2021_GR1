3182135eb7a2bc0ad824bcc4b265f7e57c3014a6
feelhardcore <tomaszgodlewski01@gmail.com>
2022-01-04 13:59:48 +0100

    handlerek postow

--- frontend/src/components/PostHandler.js
+++ frontend/src/components/PostHandler.js
@@ -11,9 +11,36 @@ export default class PostHandler{
             "sessionid": sessionid
         })
     }
-    getPost(idpost){
-        const url = `${API_URL}/posts/get/${idpost}/`;
+    getPostbyId(idpost){
+        const url = `${API_URL}/posts/get/idpost/${idpost}/`;
         return axios.get(url)
+    }
+    getuserPosts(author)
+    {
+        const url = `${API_URL}/posts/get/userposts/${author}/`;
+        return axios.get(url,{
+            "author": author
+        })
+    }
+    getselfPosts(sessionid){
+        const url = `${API_URL}/posts/get/myposts/`;
+        return axios.get(url,{
+            "sessionid": sessionid
+        })
+    }
+    deletePost(sessionid,idpost)
+    {
+        const url = `${API_URL}/posts/delete/${idpost}/`;
+        return axios.post(url,{
+            "sessionid": sessionid
+        })
+    }
+    likePost(sessionid,idpost)
+    {
+        const url = `${API_URL}/posts/like/${idpost}/`;
+        return axios.post(url,{
+            "sessionid": sessionid
+        })
     }
 
 }
