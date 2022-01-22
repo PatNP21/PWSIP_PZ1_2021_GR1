54a5500d9e3c9784ba75ba62582b426f2ea6edb5
NeverCarelessWhisper <pabelo05@o2.pl>
2022-01-22 02:08:10 +0100

    wizualne posty

--- frontend/src/components/PostList/PostItem.js
+++ frontend/src/components/PostList/PostItem.js
@@ -27,62 +27,66 @@ function PostItem(props) {
   };
 
   return (
-
     
       <div className={classes.onePostCard}>
-      <Card>
-        <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
-          <p>{props.description}</p>
-        </div>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-      </Card>
-      <Card>
-        <div className={classes.activityContent}>
-          <div className={classes.likeIt}>
-            <i>
-              <AiFillLike onClick={likeIt} />
-            </i>
-            <p>To jest OK!</p>
-          </div>
-          <div className={classes.likesCount}></div>
-          <div className={classes.writeComment}>
-            <input
-              type="text"
-              placeholder="Write a comment"
-              onChange={(e) => setComment(e.target.value)}
-            />
-            <button onClick={createComment}>Submit</button>
-          </div>
-
-          <div className={classes.comments}>
-            <ul>
-              {props.comments !== null && props.comments !== undefined ? (
-                props.comments.map((item) => {
-                  return (
-                    <li>
-                      <Card>
-                        <h4>{item.author}</h4>
-                        <p>{item.content}</p>
-                      </Card>
-                    </li>
-                  );
-                })
-              ) : (
-                <p>Nie znaleziono komentarzy</p>
-              )}
-            </ul>
-          </div>
+        <div className={classes.postContent}>
+          
+            <div className={classes.content}>
+              <h3>{props.title}</h3>
+              <address>{props.address}</address>
+              <p>{props.description}</p>
+            </div>
+            <div className={classes.image}>
+              <img src={props.image} alt={props.title} />
+            </div>
+          
         </div>
       
-      <div className={classes.actions}>
-        <button>To Favourites</button>
+        
+          <div className={classes.activityContent}>
+          
+            <div className={classes.likeIt}>
+              <i>
+                <AiFillLike onClick={likeIt} />
+              </i>
+              <p>To jest OK!</p>
+            </div>
+            <div className={classes.likesCount}></div>
+            <div className={classes.writeComment}>
+              <input
+                type="text"
+                placeholder="Write a comment"
+                onChange={(e) => setComment(e.target.value)}
+              />
+              <button onClick={createComment}>Submit</button>
+            </div>
+
+            <div className={classes.comments}>
+              <ul>
+                {props.comments !== null && props.comments !== undefined ? (
+                  props.comments.map((item) => {
+                    return (
+                      <li>
+                        <Card>
+                          <h4>{item.author}</h4>
+                          <p>{item.content}</p>
+                        </Card>
+                      </li>
+                    );
+                  })
+                ) : (
+                  <p>Nie znaleziono komentarzy</p>
+                )}
+              </ul>
+            </div>
+          </div>
+
+          <div className={classes.actions}>
+            <button>To Favourites</button>
+          </div>
+        
       </div>
-      </Card>
-    </div>
+    
   );
 }
 
